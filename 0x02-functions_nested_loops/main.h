557ef5b3c0a19ca2746dbd58a874959d9381ead2
Ekhaiakhu Daniel <ekhaiakhudaniel@gmail.com>
2022-12-14 15:08:16 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 void print_alphabeth(void);
 void print_alphabeth_x10(void);
@@ -12,6 +12,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char c);
+int _putchar(char);
 void print_alphabet(void);
+
 #endif
